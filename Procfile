32f8f076303bc137d954cb53af0d3dad14737463
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-09-22 10:51:30 -0600

    adding combined full stack code

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn voice_of_kingdom.wsgi --log-file - --bind 0.0.0.0:8000
+web: gunicorn voice_of_kingdom.wsgi --log-file - 
--- music-artist-website/src/components/Contact.js
+++ music-artist-website/src/components/Contact.js
@@ -61,7 +61,7 @@ const Contact = () => {
     setError(false);  // Reset error status
 
     try {
-      const response = await fetch('http://localhost:8000/api/contact/', {
+      const response = await fetch('/api/contact/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
