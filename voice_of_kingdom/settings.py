ee648e6d2827bb410a38faba14193c1b2b7030cf
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-09-22 11:17:57 -0600

    adding combined full stack code new

--- voice_of_kingdom/settings.py
+++ voice_of_kingdom/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = ['https://*']
 # Application definition
 CORS_ALLOWED_ORIGINS = [
     "https://*",
