67fd1dc820f73dbeb1cd44ae719da2a85cb30803
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-09-22 09:41:05 -0600

    adding combined full stack code

--- voice_of_kingdom/settings.py
+++ voice_of_kingdom/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS=[
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
