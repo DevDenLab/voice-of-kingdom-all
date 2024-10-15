ccda360d491ac4ec1bff138e85de1e1712d5fac6
Ibukun Alao <ibkalao@yahoo.com>
2024-10-14 22:16:22 -0600

    Responsivness

--- music-artist-website/src/components/Navbar/NavB.js
+++ music-artist-website/src/components/Navbar/NavB.js
@@ -1,28 +1,79 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./Navbar.css"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
- const NavB = () => {
+const NavB = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className='nav'>
-    <ul className='nav-text'>
-      <div className='navLeft'>
-        <li> <Link to = '/'>Home</Link></li>
-        <li><Link to = '/biography'>About</Link> </li>
-        <li>Home</li>
+    <div className="nav">
+      <div className="desktop-nav">
+        <ul className="nav-text">
+          <div className="navLeft">
+            <li>
+              {" "}
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/biography">About</Link>{" "}
+            </li>
+            <li>Home</li>
+          </div>
+          <div className="navMid">
+            <li className="logo">
+              <img src="/static/favicon.jpg" alt="VOKIM" />
+            </li>
+          </div>
+          <div className="navRight">
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </div>
+        </ul>
       </div>
-      <div className='navMid'>
-        <li className='logo'><img src="/static/favicon.jpg" alt="VOKIM" /></li> 
-      </div>
-      <div className='navRight'>
-        <li>Home</li>
-        <li>Home</li>
-        <li>Home</li>
-      </div>
-      
-    </ul>
-    </div>
-  )
-}
 
-export default NavB
+      {/* DRAWER NAV BAR */}
+
+      <div className="mobile-nav">
+        <div className="mobile-left">
+          <img src="/static/favicon.jpg" alt="VOKIM" />
+        </div>
+        <div className="mobile-right">
+          <button onClick={toggleDrawer} className="menu-btn">
+            <FontAwesomeIcon className="drawer" icon={faBars} />
+            {/* {isOpen ? "Close" : "Open"} */}
+          </button>
+
+          {isOpen && (
+            <>
+              <div className="side-drawer">
+                <ul className="">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/biography">About</Link>
+                  </li>
+                  <li>Home</li>
+                  <li>Home</li>
+                  <li>Home</li>
+                  <li>Home</li>
+                </ul>
+              </div>
+              <div className="overlay" onClick={toggleDrawer}>
+                <FontAwesomeIcon className="drawer" icon={faXmark} />
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavB;
